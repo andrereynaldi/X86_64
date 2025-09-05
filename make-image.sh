a638800ae47578d199923f867f853b1edeb699ef
@sogek1ng__ <andrereynaldi99@gmail.com>
2025-09-05 16:49:50 +0700

    Update make-image.sh

--- make-image.sh
+++ make-image.sh
@@ -15,6 +15,9 @@ PACKAGES=""
 MISC=""
 EXCLUDED=""
 
+#Base
+PACKAGES+="
+
 # Core system + Web Server + LuCI
 PACKAGES+=" dnsmasq libc bash block-mount coreutils-base64 coreutils-sleep coreutils-stat coreutils-stty \
 curl wget-ssl tar unzip parted losetup uhttpd uhttpd-mod-ubus luci luci-base \
@@ -86,11 +89,16 @@ configure_profile_packages() {
     fi
 }
 
-# Packages Base
+# Packages Base Firmware Selector
 configure_release_packages() {
     if [[ "${BASE:-}" == "openwrt" ]]; then
-        MISC+=" wpad-openssl iw iwinfo wireless-regdb kmod-cfg80211 kmod-mac80211 luci-app-temp-status"
-        EXCLUDED+=""
+        MISC+=" base-files ca-bundle dnsmasq dropbear e2fsprogs firewall4 \
+        fstools grub2-bios-setup kmod-button-hotplug kmod-nft-offload libc \
+        libgcc libustream-mbedtls logd mkf2fs mtd netifd nftables odhcp6c odhcpd-ipv6only \
+        opkg partx-utils ppp ppp-mod-pppoe procd-ujail uci uclient-fetch urandom-seed \
+        urngd kmod-e1000e kmod-e1000 \
+        kmod-fs-vfat kmod-igb kmod-r8169 kmod-drm-i915 luci"
+        EXCLUDED+=" kmod-amazon-ena kmod-amd-xgbe kmod-bnx2 kmod-dwmac-intel kmod-forcedeth kmod-igc kmod-ixgbe kmod-tg3"
     elif [[ "${BASE:-}" == "immortalwrt" ]]; then
         MISC+=" wpad-openssl iw iwinfo wireless-regdb kmod-cfg80211 kmod-mac80211"
         EXCLUDED+=" -dnsmasq -cpusage -automount -libustream-openssl -default-settings-chn -luci-i18n-base-zh-cn"
